0253fbf1d64a1939f3453ff0897c1779e2238fe5
Asecq <sec.alsha1b@gmail.com>
2022-05-22 15:22:10 +0300

    last update

--- api/connect.js
+++ api/connect.js
@@ -1,7 +1,7 @@
 //Import Librarie mongoose
 const mongodb = require("mongoose");
 
-mongodb.connect('mongodb://localhost/products')
+mongodb.connect('mongodb+srv://dev:6HIhC0jwAlraiLuG@cluster0.lljoq.mongodb.net/?retryWrites=true&w=majority')
 .then(()=>console.log("Connected Successfuly"))
 .catch(()=>console.log("Not Connect"));
 
